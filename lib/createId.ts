5a3616eb3d005fc58cd3a7776b75f6c298697af8
Snorkungen <51717522+Snorkungen@users.noreply.github.com>
2022-08-27 12:22:45 +0300

    Update createId.ts

--- lib/createId.ts
+++ lib/createId.ts
@@ -1,8 +1,10 @@
-const ID_LENGTH = 30;
+const ID_LENGTH = 16;
 
-export default function createId(num: number) {
-    let buffer = (Buffer.from(num + "")).toString("hex");
-    let id = "" + num + Math.random().toFixed(3).substring(2);
+export default function createId(num: number, id_len = ID_LENGTH) {
+    if (id_len > 98) throw "ID length to large; max size 98";
 
-    return Buffer.from(id).toString("base64");
+    let id = num.toString(16)
+    id += Math.random().toFixed(id_len - id.length + 2).substring(2)
+
+    return id;
 }
